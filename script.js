7e90d559aba7ae472440f0ed8c50b4b8bb165668
FerdiansahSukara <103658352+FerdiansahSukarya@users.noreply.github.com>
2025-11-25 12:48:30 +0700

    Upload semua file website

---
+++ script.js
@@ -0,0 +1,13 @@
+// Initialize AOS (Animate On Scroll)
+AOS.init({
+    once: true, 
+    offset: 100,
+});
+
+// Initialize Typed.js (Efek Mengetik)
+var typed = new Typed('#typed-output', {
+    strings: ['Websites.', 'UI/UX Designs.'],
+    typeSpeed: 50,
+    backSpeed: 30,
+    loop: true
+});
